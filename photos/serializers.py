c3b997e17d6e8641735e6d747928fae576468969
Artem P <119848942+riikinkukko@users.noreply.github.com>
2025-05-16 20:58:51 +0300

    Delete photos directory

--- photos/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Photo
-
-class PhotoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Photo
-        fields = ['id', 'image', 'description', 'date_taken', 'latitude', 'longitude', 'name']
